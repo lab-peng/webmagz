52eea7963c3febccd768c55b46c6d00f6c60d425
Robert <panerobert@yahoo.com>
2021-12-19 16:38:01 +0800

    'm'

--- webmagz/context_processors.py
+++ webmagz/context_processors.py
@@ -3,8 +3,8 @@ from blog.models import Category, Blog
 
 def contexts(request):
     categories = Category.objects.all()
-    recent_posts = Blog.objects.all()[0:4]
-    recent_post = Blog.objects.all()[0]
+    recent_posts = Blog.objects.filter(pk__in=[1, 2, 3, 4])
+    recent_post = Blog.objects.first()
     # print(request.method)
     context = {
         'categories': categories,
